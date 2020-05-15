3bb05745107099603721267dde475cd810686fd1
Evgen-L <e.liakishev@gmail.com>
2020-05-15 08:38:01 +0300

    Исправление замечаний

--- Lw13/Task 3/13dot3(final version).pas
+++ Lw13/Task 3/13dot3(final version).pas
@@ -2,8 +2,8 @@ PROGRAM Split(INPUT,OUTPUT);
   {Копирует INPUT в OUTPUT,сначала нечетные,а затем четные
    элементы}
 VAR
-  Ch,Next: CHAR;
-  Odds,Evens: TEXT;
+  Ch, Next: CHAR;
+  Odds, Evens: TEXT;
 {PROCEDURE CopyOut(VAR F1: TEXT; VAR Ch: CHAR);}
 PROCEDURE CopyOut(VAR F1: TEXT; VAR Ch: CHAR);
 BEGIN{CopyOut}
@@ -21,32 +21,36 @@ BEGIN {Split}
   {Разделяет INPUT в Odds и Evens}
   ASSIGN(Odds,'Odds.dat');
   ASSIGN(Evens,'Evens.dat');
-  REWRITE(Odds);
-  REWRITE(Evens);
-  Next := 'O';
-  WHILE NOT EOLN(INPUT)
+  WHILE NOT EOF
   DO
-  {Прочитать Ch, записать в файл, выбранный через
-  Next,переключить Next}
     BEGIN
-      READ(INPUT, Ch);
-      IF Next = 'O'
-      THEN
+      REWRITE(Odds);
+      REWRITE(Evens);
+      Next := 'O';
+      WHILE NOT EOLN(INPUT)
+      DO
+       {Прочитать Ch, записать в файл, выбранный через
+       Next,переключить Next}
         BEGIN
-          Next := 'E';
-          WRITE(Odds, Ch)
-        END
-      ELSE
-        BEGIN
-          Next := 'O';
-          WRITE(Evens, Ch)
-        END;           
-    END;
-  READLN(INPUT);
-  WRITELN(Odds);
-  WRITELN(Evens);
-  CopyOut(Odds,Ch);
-  CopyOut(Evens,Ch);
-  WRITELN
+          READ(INPUT, Ch);
+          IF Next = 'O'
+          THEN
+            BEGIN
+              Next := 'E';
+              WRITE(Odds, Ch)
+            END
+          ELSE
+            BEGIN
+              Next := 'O';
+              WRITE(Evens, Ch)
+            END;           
+        END;
+      READLN(INPUT);
+      WRITELN(Odds);
+      WRITELN(Evens);
+      CopyOut(Odds,Ch);
+      CopyOut(Evens,Ch);
+      WRITELN
+    END
 END.{Split}
 
